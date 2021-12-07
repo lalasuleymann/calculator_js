31f0d4bc4a8a4b4d2693ea669df884f6c545287a
Your Name <you@example.com>
2021-12-07 11:14:39 +0400

    hw-calculator

---
+++ calculator/js/index.js
@@ -0,0 +1,19 @@
+let a=20;
+let b=2;
+
+function Sum(a,b) {
+    return a+b;
+}
+function Substraction(a,b) {
+    return a-b;
+}
+function Multiplication(a,b) {
+    return a*b;
+}
+function Division(a,b) {
+    return a/b;
+}
+console.log("Sum: ",Sum(a,b));
+console.log("Substraction: ",Substraction(a,b));
+console.log("Multiplication: ",Multiplication(a,b));
+console.log("Division: ",Division(a,b));
